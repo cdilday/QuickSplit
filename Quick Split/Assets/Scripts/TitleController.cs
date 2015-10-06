38359910791d801c138ad7d3b73243d772122c03
cdilday <cdilday@ucsc.edu>
2015-10-06 14:32:10 -0700

    Main Menu Update:
    - Added a way of switching between multiple screens
    - Added a Credits scene, will need to be updated as more people contribute. May also need a better copyright explanation
    - Added a how-to-play screen with a summary and controls
    - Created and added images for the control schemes on PC. Will need to be able to detect and change for the mobile version
    
    Next update will implement music.

--- Quick Split/Assets/Scripts/TitleController.cs
+++ Quick Split/Assets/Scripts/TitleController.cs
@@ -3,19 +3,55 @@ using System.Collections;
 
 public class TitleController : MonoBehaviour {
 
+	public GameObject gameModeLayer;
+	public GameObject[] howToPlayLayers;
+	public GameObject creditsLayer;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		Goto_Game_Mode_Layer ();
 	}
 
+	//loads the proper game mode into the playerprefs for the game scene to read, then loads the game scene
+	//This is where the UI for the transition would go
 	public void Load_Game(int gameMode)
 	{
 		PlayerPrefs.SetInt("Mode", gameMode);
 		Application.LoadLevel("Game Scene");
 	}
+
+	//loads the game mode layer and unloads the other layers
+	public void Goto_Game_Mode_Layer()
+	{
+		gameModeLayer.SetActive (true);
+		howToPlayLayers[0].SetActive (false);
+		howToPlayLayers[1].SetActive (false);
+		creditsLayer.SetActive (false);
+	}
+
+	//loads the How to Play Layer and unloads the other layers
+	public void Goto_How_To_Play_Layer()
+	{
+		howToPlayLayers[0].SetActive (true);
+		howToPlayLayers[1].SetActive (false);
+		gameModeLayer.SetActive (false);
+		creditsLayer.SetActive (false);
+	}
+
+	public void Goto_Controls_Layer()
+	{
+		howToPlayLayers[0].SetActive (false);
+		howToPlayLayers[1].SetActive (true);
+		gameModeLayer.SetActive (false);
+		creditsLayer.SetActive (false);
+	}
+
+	//loads the Credits layer and unloads the other layers
+	public void Goto_Credits_Layer()
+	{
+		creditsLayer.SetActive (true);
+		gameModeLayer.SetActive (false);
+		howToPlayLayers[0].SetActive (false);
+		howToPlayLayers[1].SetActive (false);
+	}
+
 }
