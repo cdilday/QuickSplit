6e903d3bfac1e604a48367204d40c03e0f7b4f38
cdilday <cdilday@ucsc.edu>
2015-10-20 17:46:10 -0700

    Title Screen UI and Build Updates:
    - Updated the title screen to resize better with different resolutions, but still needs work
    - build settings for webgl updated.

---
+++ Quick Split/Assets/Scripts/Layer_Resizer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layer_Resizer : MonoBehaviour {
+
+	public RectTransform parentCanvas;
+	RectTransform rectTransform;
+
+	// Use this for initialization
+	void Start () {
+		rectTransform = gameObject.GetComponent<RectTransform> ();
+		rectTransform.position = parentCanvas.position;
+		rectTransform.sizeDelta = parentCanvas.sizeDelta;
+
+	}
+}
---
+++ Quick Split/Assets/Scripts/Sound_Effector.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Sound_Effector : MonoBehaviour {
+
+	//for sfx that need to play often in relation to unstable objects
+
+	//attatch as many audiosources as the maximum number of sounds  may need toplay at once
+	List<AudioSource> aSources;
+	//marks which ones are and aren't busy
+	List<bool> availableSource;
+
+	//for pitch variations
+	public bool variation;
+	public float variationAmount;
+
+	// Use this for initialization
+	void Start () {
+		aSources = new List<AudioSource> ();
+		availableSource = new List<bool> ();
+		//get all attatched sources
+		AudioSource[] attatchedSources = gameObject.GetComponents<AudioSource>();
+
+		//load them up properly
+		foreach (AudioSource source in attatchedSources) {
+			aSources.Add(source);
+			availableSource.Add (true);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// upkeep for available sources
+		for (int i = 0; i < aSources.Count(); i++) {
+			if(!availableSource.ElementAt (i))
+			{
+				if(!aSources.ElementAt(i).isPlaying){
+					availableSource[i] = true;
+				}
+			}
+		}
+	}
+
+	void PlaySound(){
+
+		for (int i = 0; i < aSources.Count(); i++){
+			//find first available audiosource
+			if (availableSource.ElementAt (i)) {
+				availableSource[i] = false;
+				//set proper volume
+				aSources[i].volume = PlayerPrefs.GetFloat ("SFX Volume", 1);
+
+				//add variation if needed
+				if(variation)
+				{
+					aSources[i].pitch = 1f + Random.Range (-1f * variationAmount, variationAmount);
+				}
+				aSources.ElementAt(i).Play ();
+			}
+		}
+	}
+}
