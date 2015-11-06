f9b057e13767980720c351ebd79edd6176e7c81d
cdilday <cdilday@ucsc.edu>
2015-11-06 15:00:20 -0800

    Script reorganizing, piece improvements and bugfixes:
    - reorganized the scripts into more sensible folders
    - the piece pulser is now back in front of the piece but is more efficiently activated and deactivated so it never blocks the sprite when not active
    - fixed the bug where pieces in the holder were not selectable during spells

--- Quick Split/Assets/Scripts/Game Screen/Piece Related/Piece_Pulser.cs
+++ Quick Split/Assets/Scripts/Game Screen/Piece Related/Piece_Pulser.cs
@@ -5,23 +5,41 @@ public class Piece_Pulser : MonoBehaviour {
 
 	public SpriteRenderer spriteRenderer;
 	piece_script parentPiece;
+
+	Color activeColor;
+	Color inActiveColor;
+
+	bool isPulsing;
 	// Use this for initialization
 	void Start () {
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 		parentPiece = transform.GetComponentInParent<piece_script> ();
 		spriteRenderer.sprite = parentPiece.GetComponent<SpriteRenderer> ().sprite;
-		spriteRenderer.color = new Color (spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.5f);
-		//move slightly behind the piece so as not to block it
-		transform.Translate(new Vector3(0,0,0.1f));
+		activeColor = new Color (spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.5f);
+		inActiveColor = new Color (spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0);
+		spriteRenderer.color = inActiveColor;
+		isPulsing = false;
+		//move slightly in front of the so as to make it obvious when it's active
+		transform.Translate (new Vector3 (0, 0, -0.1f));
 	}
 
 	void FixedUpdate () {
 		//check if this pulser should be active
 		if (!parentPiece.selectable && parentPiece.gridPos.y != 6 && parentPiece.gridPos.y != 9) {
 			transform.localScale = Vector3.one;
+			//set to inactivecolor only on the frame after it was active
+			if(isPulsing)
+			{
+				isPulsing = false;
+				spriteRenderer.color = inActiveColor;
+			}
 			return;
 		}
-
+		//set to Active color if it was previously inactive
+		if (!isPulsing) {
+			isPulsing = true;
+			spriteRenderer.color = activeColor;
+		}
 		Vector3 newScale = transform.localScale;
 		newScale = new Vector3 ((Mathf.Sin (Time.time * 4 ) *0.25f) + 1.25f, (Mathf.Sin (Time.time * 4 ) *0.25f) + 1.25f, newScale.z);
 		transform.localScale = newScale;
