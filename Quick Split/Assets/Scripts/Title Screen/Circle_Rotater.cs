ff64e25afc37e5db55b7343070fceab9662c66a2
cdilday <cdilday@ucsc.edu>
2015-12-30 18:22:32 -0800

    New title logo and gear elements added to the title screen.

--- Quick Split/Assets/Scripts/Title Screen/Circle_Rotater.cs
+++ Quick Split/Assets/Scripts/Title Screen/Circle_Rotater.cs
@@ -8,20 +8,28 @@ public class Circle_Rotater : MonoBehaviour {
 
 	RectTransform rectTransform;
 
+	public float speed;
+	public bool randomize;
+
 	public bool Clockwise;
 
 	void Start()
 	{
 		rectTransform = GetComponent<RectTransform> ();
+		if (randomize) {
+			speed = Random.Range(-10f, 10f);
+		}
+		if (speed == 0)
+			speed = 1f;
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		if(Clockwise){
-			rectTransform.Rotate(new Vector3(0,0,1f));
+			rectTransform.Rotate(new Vector3(0,0,speed));
 		}
 		else {
-			rectTransform.Rotate(new Vector3(0,0,-1f));
+			rectTransform.Rotate(new Vector3(0,0,speed));
 		}
 	}
 }
