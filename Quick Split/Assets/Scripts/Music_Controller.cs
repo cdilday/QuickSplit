f4e8e4c44a843ba642615726296101fd4134118f
cdilday <cdilday@ucsc.edu>
2016-01-15 18:11:51 -0800

    Updated Music and Music features:
    - Added new Music files for all 4 game tracks, game over music, menu music, and 2 variations of ticking for each game music
    - began implementing new ticking feature as well as new music for game screens

--- Quick Split/Assets/Scripts/Music_Controller.cs
+++ Quick Split/Assets/Scripts/Music_Controller.cs
@@ -8,12 +8,30 @@ public class Music_Controller : MonoBehaviour {
 	public float musicVolume;
 	public float SFXVolume;
 
-	public AudioClip WitMusic;
-	public AudioClip QuickMusic;
 	public AudioClip WizMusic;
+	public AudioClip QuickMusic;
+	public AudioClip WitMusic;
 	public AudioClip HolyMusic;
 
+	// 0 is the slow tick, 1 is the fast tick
+	public AudioClip[] WizTicks;
+	public AudioClip[] QuickTicks;
+	public AudioClip[] WitTicks;
+	public AudioClip[] HolyTicks;
+
+	/* Song titles -> modes:
+	 * Cog Capers = Quick
+	 * Resistance March = Holy
+	 * Satellite = Wit
+	 * The Flying Machine = Wiz
+	 * */
+
 	AudioSource MusicSource;
+	public AudioSource SlowTickSource;
+	public AudioSource FastTickSource;
+
+	bool isSlowTicking = false;
+	bool isFastTicking = false;
 
 	void Awake(){
 		//get rid of redundant music controllers, there should always be one but only one
@@ -36,15 +54,23 @@ public class Music_Controller : MonoBehaviour {
 			switch (GameObject.Find("Gamecontroller").GetComponent<GameController>().gameType){
 			case "Wit":
 				MusicSource.clip = WitMusic;
+				SlowTickSource.clip = WitTicks[0];
+				FastTickSource.clip = WitTicks[1];
 				break;
 			case "Quick":
 				MusicSource.clip = QuickMusic;
+				SlowTickSource.clip = QuickTicks[0];
+				FastTickSource.clip = QuickTicks[1];
 				break;
 			case "Wiz":
 				MusicSource.clip = WizMusic;
+				SlowTickSource.clip = WizTicks[0];
+				FastTickSource.clip = WizTicks[1];
 				break;
 			case "Holy":
 				MusicSource.clip = HolyMusic;
+				SlowTickSource.clip = HolyTicks[0];
+				FastTickSource.clip = HolyTicks[1];
 				break;
 			default:
 				break;
@@ -52,7 +78,27 @@ public class Music_Controller : MonoBehaviour {
 		}
 
 		MusicSource.volume = PlayerPrefs.GetFloat ("Music Volume", 1);
+		musicVolume = MusicSource.volume;
+		SlowTickSource.volume = musicVolume;
+		FastTickSource.volume = musicVolume;
 		SFXVolume = PlayerPrefs.GetFloat ("SFX Volume", 1);
+
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Keypad1)){
+			Start_Slow_Tick();
+		}
+		else if(Input.GetKeyDown(KeyCode.Keypad2)){
+			Stop_Slow_Tick();
+		}
+		else if(Input.GetKeyDown(KeyCode.Keypad3)){
+			Start_Fast_Tick();
+		}
+		else if(Input.GetKeyDown(KeyCode.Keypad4)){
+			Stop_Fast_Tick();
+		}
 	}
 
 	//stops the music
@@ -65,12 +111,24 @@ public class Music_Controller : MonoBehaviour {
 	public void Pause_Music()
 	{
 		MusicSource.Pause ();
+		if (isSlowTicking) {
+			SlowTickSource.Pause ();
+		}
+		if (isFastTicking) {
+			FastTickSource.Pause ();
+		}
 	}
 
 	//unpauses the music
 	public void Resume_Music()
 	{
 		MusicSource.UnPause ();
+		if (isSlowTicking) {
+			SlowTickSource.UnPause ();
+		}
+		if (isFastTicking) {
+			FastTickSource.UnPause ();
+		}
 	}
 
 	//plays the musc for the given game type
@@ -79,15 +137,23 @@ public class Music_Controller : MonoBehaviour {
 		switch (gameType){
 		case "Wit":
 			MusicSource.clip = WitMusic;
+			SlowTickSource.clip = WitTicks[0];
+			FastTickSource.clip = WitTicks[1];
 			break;
 		case "Quick":
 			MusicSource.clip = QuickMusic;
+			SlowTickSource.clip = QuickTicks[0];
+			FastTickSource.clip = QuickTicks[1];
 			break;
 		case "Wiz":
 			MusicSource.clip = WizMusic;
+			SlowTickSource.clip = WizTicks[0];
+			FastTickSource.clip = WizTicks[1];
 			break;
 		case "Holy":
 			MusicSource.clip = HolyMusic;
+			SlowTickSource.clip = HolyTicks[0];
+			FastTickSource.clip = HolyTicks[1];
 			break;
 		default:
 			break;
@@ -125,6 +191,32 @@ public class Music_Controller : MonoBehaviour {
 	{
 		musicVolume = value;
 		MusicSource.volume = musicVolume;
+		SlowTickSource.volume = musicVolume;
+		FastTickSource.volume = musicVolume;
+	}
+
+	public void Start_Slow_Tick(){
+		float startTime = MusicSource.time;
+		SlowTickSource.Play ();
+		SlowTickSource.timeSamples = MusicSource.timeSamples;
+		isSlowTicking = true;
+	}
+
+	public void Stop_Slow_Tick(){
+		SlowTickSource.Stop();
+		isSlowTicking = false;
+	}
+
+	public void Start_Fast_Tick(){
+		float startTime = MusicSource.time;
+		FastTickSource.Play ();
+		FastTickSource.timeSamples = MusicSource.timeSamples;
+		isFastTicking = true;
+	}
+	
+	public void Stop_Fast_Tick(){
+		FastTickSource.Stop ();
+		isFastTicking = false;
 	}
 
 }
