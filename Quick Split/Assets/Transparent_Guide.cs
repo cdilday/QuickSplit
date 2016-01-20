f702ad2f6d175479e909babcff5d69d7e7240a66
cdilday <cdilday@ucsc.edu>
2016-01-19 18:29:29 -0800

    Added a transparency region guide for more intuitive controls as part of a way to improve clarity. Will be disable-able and will matches the how to play section

---
+++ Quick Split/Assets/Transparent_Guide.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Transparent_Guide : MonoBehaviour {
+
+	Image image;
+	Color defaultColor;
+	RectTransform rectTransform;
+	Camera mainCamera;
+
+	bool beingTouched;
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image> ();
+		defaultColor = image.color;
+		rectTransform = GetComponent<RectTransform> ();
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+		//put resizing code here
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		beingTouched = false;
+		foreach (Touch poke in Input.touches) {
+			if(RectTransformUtility.RectangleContainsScreenPoint(rectTransform, poke.position, mainCamera)){
+				image.color = new Color(image.color.r, image.color.g, image.color.b, 0.33f);
+				beingTouched = true;
+			}
+		}
+		if (!beingTouched) {
+			image.color = defaultColor;
+		}
+	}
+}
