35ebf319beae656fc825ffe352a0a8d1614bf139
cdilday <cdilday@ucsc.edu>
2016-10-28 12:08:06 -0700

    Began work on Kong Build. Will need to:
    - make achievements send info to the Kong Controller
    - make scores send info to the Kong Controller
    - Pull scores off of Kongregate and populate the score table accordingly

---
+++ Quick Split/Assets/KongController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KongController : MonoBehaviour {
+	
+	private static KongController instance;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		//get rid of redundant kong controllers, there should always be one but only one
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		Application.ExternalEval(
+      		@"if(typeof(kongregateUnitySupport) != 'undefined'){
+        	kongregateUnitySupport.initAPI('KongregateAPI', 'OnKongregateAPILoaded');
+      		};"
+    	);
+	}
+
+	//
+	public void Submit_Score(string gameType, int score){
+		Application.ExternalCall("kongregate.stats.submit", gameType, score);
+	}
+
+	//No real difference between this and submit score other than this is only for sending if an achievement has unlocked to kong
+	public void Submit_Achievement (string achievename){
+		Application.ExternalCall("kongregate.stats.submit", achievename, 1);
+	}
+
+}
